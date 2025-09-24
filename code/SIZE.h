0d91aeedacb60bf5d2004cb8044223920f72fa92
ZhangHang <zhanghang@qdio.ac.cn>
2025-09-24 13:10:08 +0800

    modified:   code/SIZE.h 	modified:   input/data 	modified:   input/data.exf 	modified:   input/data.obcs 	modified:   script/analysis.ipynb

--- code/SIZE.h
+++ code/SIZE.h
@@ -44,8 +44,8 @@ CEOP
       PARAMETER (
      &           sNx =  168,
      &           sNy =  36,
-     &           OLx =   2,
-     &           OLy =   2,
+     &           OLx =   3,
+     &           OLy =   3,
      &           nSx =   1,
      &           nSy =   1,
      &           nPx =   1,
