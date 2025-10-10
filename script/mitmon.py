b7919a21096744244179dc28dbed9b531ad36fce
ZhangHang <zhanghang@qdio.ac.cn>
2025-10-10 10:15:11 +0800

    增加bash监控脚本

--- script/mitmon.py
+++ script/mitmon.py
@@ -5,17 +5,23 @@ ts = slice(None, None)
 paths = find_mon_file()
 key = ['momKE_ave','ETAN_std']
 # key.append('RHOAnoma_lv_std')
-
+isbreak = False
 with xr.open_mfdataset(paths) as a:
     print(f"目前迭代步数为{a['T'].size}, 时间为{a['T'].values[-1]}")
     for k in key:
         if k not in a:
             raise ValueError(f"变量{k}不在数据集中,可用变量有{list(a.data_vars)}")
-        a[k].loc[ts].plot(marker='.',label=k)
-
+        da = a[k].loc[ts].load()
+        if da[-1].isnull().item():
+            isbreak = True
+        da.plot(marker='.',label=k)
+if isbreak:
+    print("注意: 数据出现null值, 可能已经崩溃!!!")
+else:
+    print("数据正常, 未出现null值")
 plt.gca().set_yscale('log')
 plt.legend()
 plt.ylabel('')
 plt.title(f"nIter:{a['T'].size}")
-plt.savefig('监控.jpg')
+plt.savefig('监控.jpg', dpi=300)
 print("图片已保存为:监控.jpg")
