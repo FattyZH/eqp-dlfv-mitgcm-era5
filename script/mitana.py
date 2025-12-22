0018ba229ee2cc7e3b55ebeaa2948cc0347c1ee8
ZhangHang <zhanghang@qdio.ac.cn>
2025-12-22 12:22:51 +0800

    rwSuffixType取2会导致68年爆int，改为默认用迭代数作为文件名

--- script/mitana.py
+++ script/mitana.py
@@ -9,8 +9,8 @@ outdir = '../output/'
 dirs = [p for p in Path(outdir).iterdir() if p.is_dir()]
 dirs.sort()
 path = dirs[-1]
-
-a = open_mds(path,prefix=['U','V','T','S'],ref_date='1940-01-16')
+name = path.name
+a = open_mds(path,prefix=['dync'],ref_date='1940-01-16')
 print(a)
 # id = a.indexes['T'].get_loc('1940-03-01 00:00:00')
 
@@ -18,25 +18,25 @@ id = -1
 lev = 0
 plt.figure(figsize=(12,12))
 plt.subplot(4,1,1)
-a['U'][id,lev].plot(cmap='RdBu_r',vmin=-1, vmax=1)
+a['UVEL'][id,lev].plot(cmap='RdBu_r',vmin=-1, vmax=1)
 plt.subplot(4,1,2)
-a['V'][id,lev].plot(cmap='RdBu_r',vmin=-1, vmax=1)
+a['VVEL'][id,lev].plot(cmap='RdBu_r',vmin=-1, vmax=1)
 plt.subplot(4,1,3)
-a['T'][id,lev].plot(cmap='RdBu_r',vmin=15, vmax=35)
+a['THETA'][id,lev].plot(cmap='RdBu_r',vmin=15, vmax=35)
 plt.subplot(4,1,4)
-a['S'][id,lev].plot(cmap='RdBu_r',vmin=30, vmax=38)
-plt.savefig(Path.joinpath(path,'_state_last.png'),dpi=300)
+a['SALT'][id,lev].plot(cmap='RdBu_r',vmin=30, vmax=38)
+plt.savefig(outdir+name+'_state_last.png',dpi=300)
 
 
 id = slice(-12,None)
 lev = 0
 plt.figure(figsize=(12,12))
 plt.subplot(4,1,1)
-a['U'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=-1, vmax=1)
+a['UVEL'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=-1, vmax=1)
 plt.subplot(4,1,2)
-a['V'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=-1, vmax=1)
+a['VVEL'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=-1, vmax=1)
 plt.subplot(4,1,3)
-a['T'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=15, vmax=35)
+a['THETA'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=15, vmax=35)
 plt.subplot(4,1,4)
-a['S'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=30, vmax=38)
-plt.savefig(Path.joinpath(path,'_state_mean.png'),dpi=300)
+a['SALT'][id,lev].mean('time').plot(cmap='RdBu_r',vmin=30, vmax=38)
+plt.savefig(outdir+name+'_state_mean.png',dpi=300)
