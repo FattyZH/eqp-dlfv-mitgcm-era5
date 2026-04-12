34c23ee13a6e73b5aceee1177db4873704876c49
ZhangHang <zhanghang@qdio.ac.cn>
2026-04-12 11:25:38 +0800

    analysis code

--- script/analysis/kpp_ana.py
+++ script/analysis/kpp_ana.py
@@ -10,14 +10,28 @@ import xarray as xr
 latlim = slice(-5.5,5.5)
 import mit_utils
 
-exp = '260409_162411'
-hbl = mit_utils.open_mds('../../output/'+exp,prefix='KPPhbl')
-viscAz = mit_utils.open_mds('../../output/'+exp,prefix='KPPviscAz')
-#%%
+exp = '260410_164626'
+hbl = mit_utils.open_mds('../../output/'+exp,prefix='KPPhbl').sel(YC=slice(-2,2))
+viscAz = mit_utils.open_mds('../../output/'+exp,prefix='KPPviscAz').sel(YC=slice(-2,2))
 display(viscAz)
-ind = 9+12*16
+#%%
+ind = slice(-130,-10)
 plt.figure(figsize=(10,8))
 plt.subplot(211)
-hbl['KPPhbl'][ind].plot(cmap='RdYlBu_r',vmax=120)
+hblm = hbl['KPPhbl'][ind].mean('YC')
+hblm.mean('time').plot()
 plt.subplot(212)
-viscAz['KPPviscAz'][ind,1].plot(cmap='RdYlBu_r',vmax=0.03)
+viscAzm = viscAz['KPPviscAz'][ind,:].mean('YC')
+viscAzm.mean('time').plot(cmap='RdYlBu_r')
+#%%
+plt.contourf(hblm.values.reshape(10,12,-1).mean(0))
+plt.colorbar()
+#%%
+import matplotlib.colors as colors
+
+kzs = viscAzm.values.reshape(-1,12,viscAzm.shape[1],viscAzm.shape[2]).mean(0)
+for i in range(0,12,3):
+    plt.subplot(2,2,i+1)
+    plt.contourf(np.log10(kzs[3,:,:]), levels=np.linspace(-5.5, -1.5, 9),extend='both', cmap='RdYlBu_r')
+    plt.gca().invert_yaxis()
+    plt.colorbar()
