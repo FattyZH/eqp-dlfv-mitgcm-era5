fdf4ea7fb623702b9da3b0876fb3082c3971c327
ZhangHang <zhanghang@qdio.ac.cn>
2026-04-12 11:26:54 +0800

    Ibid.

--- script/analysis/kpp_ana.py
+++ script/analysis/kpp_ana.py
@@ -24,14 +24,32 @@ plt.subplot(212)
 viscAzm = viscAz['KPPviscAz'][ind,:].mean('YC')
 viscAzm.mean('time').plot(cmap='RdYlBu_r')
 #%%
-plt.contourf(hblm.values.reshape(10,12,-1).mean(0))
+from scipy.interpolate import interp1d
+hbls = hblm.values.reshape(10,12,-1).mean(0)
+plt.contourf(hbls)
 plt.colorbar()
+z = viscAz['Zl'].values
+x = viscAz['XC'].values
+zp1 = viscAz['Zp1'].values
+hbn = interp1d(-zp1,np.arange(zp1.size))(hbls)
 #%%
 import matplotlib.colors as colors
 
 kzs = viscAzm.values.reshape(-1,12,viscAzm.shape[1],viscAzm.shape[2]).mean(0)
+cnt = 0
+plt.figure(figsize=(10,14))
 for i in range(0,12,3):
-    plt.subplot(2,2,i+1)
-    plt.contourf(np.log10(kzs[3,:,:]), levels=np.linspace(-5.5, -1.5, 9),extend='both', cmap='RdYlBu_r')
+    cnt+=1
+    plt.subplot(4,1,cnt)
+    plt.contourf(x,-z,np.log10(kzs[i,:,:]), levels=np.linspace(-5.5, -1.5, 4*4+1),extend='both', cmap='RdYlBu_r')
+    plt.plot(x,hbls[i])
     plt.gca().invert_yaxis()
     plt.colorbar()
+
+#%%
+i,ix = 3,400
+plt.plot(np.log10(kzs[i,:,ix]),-z,marker='.')
+plt.axhline(hbls[i,ix],color='k')
+plt.axhline(115,color='k')
+plt.xlim(-6,-1.5)
+plt.ylim(500,0)
